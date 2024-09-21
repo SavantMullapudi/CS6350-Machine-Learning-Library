af5c7faaf242bbcec26b1132cd1538e04b9ca558
Savant Mullapudi <u1471518@utah.edu>
2024-09-20 19:49:17 -0600

    Update run.sh

--- DecisionTree/run.sh
+++ DecisionTree/run.sh
@@ -6,3 +6,5 @@ python3 car_decision_tree.py
 echo "Bank dataset train / test errors. Press 1 for unknowns to be replaced and 0 for unknowns to remain same"
 python3 bank_decision_tree.py
 
+echo "Bank dataset train / test errors. Press 1 for unknowns to be replaced and 0 for unknowns to remain same"
+python3 bank_decision_tree.py
