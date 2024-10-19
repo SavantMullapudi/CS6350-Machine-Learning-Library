eef49ff5e6a8747fc75049ccd6a07867af67e516
Savant Mullapudi <u1471518@utah.edu>
2024-10-18 22:54:35 -0600

    Create run.sh

---
+++ Linear Regression/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Batch Gradient Descent"
+python3 batch_grad_descent.py
+
+echo "Stochastic Gradient Descent"
+python3 stochastic_grad_descent.py
+
+echo "Optimal Weight"
+python3 optimal_wt.py
