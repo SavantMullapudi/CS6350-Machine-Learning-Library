99b782c1b2e0a595d618fc2b7ff08671a9ee6e0c
Savant Mullapudi <u1471518@utah.edu>
2024-10-18 20:33:56 -0600

    Update batch_grad_descent.py

--- Linear Regression/batch_grad_descent.py
+++ Linear Regression/batch_grad_descent.py
@@ -2,8 +2,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-train_data = pd.read_csv('DecisionTree/data/concrete/train.csv')
-test_data = pd.read_csv('DecisionTree/data/concrete/test.csv')
+train_data = pd.read_csv('data/concrete/train.csv')
+test_data = pd.read_csv('data/concrete/test.csv')
 
 X_train = train_data.drop(columns=train_data.columns[-1]).values  
 y_train = train_data.iloc[:, -1].values  
@@ -41,7 +41,6 @@ max_iterations = 100000
 
 optimal_wts, history_of_cost = run_grad_descent(X_train_bias, y_train, initial_wts, learning_rate, tolerance, max_iterations)
 
-
 plt.plot(history_of_cost, color='orange')  
 plt.xlabel('Iterations', fontsize=12) 
 plt.ylabel('Cost', fontsize=12)  
@@ -49,12 +48,9 @@ plt.title('Cost vs Iterations Comparison', fontsize=14)
 plt.grid(False)  
 plt.show()
 
-
 y_pred_test = np.dot(X_test_bias, optimal_wts)  
 
-
 mse_test = (1/(2*len(y_test))) * np.sum((y_pred_test - y_test)**2)
-
 
 print("Optimized Wts:", optimal_wts)
 print("Learning Rate:", learning_rate)
