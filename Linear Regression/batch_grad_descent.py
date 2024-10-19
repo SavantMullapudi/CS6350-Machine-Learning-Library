bf2163e32aa326c985c6b90e901807aa3d95a38d
Savant Mullapudi <u1471518@utah.edu>
2024-10-18 21:03:14 -0600

    Update batch_grad_descent.py

--- Linear Regression/batch_grad_descent.py
+++ Linear Regression/batch_grad_descent.py
@@ -31,7 +31,7 @@ def run_grad_descent(X, y, init_wts, lr, threshold, max_iters):
 
         wts = updated_wts
 
-    return wts, cost_history, iteration  
+    return wts, cost_history, iteration 
 
 initial_wts = np.zeros(X_train_bias.shape[1]) 
 tolerance = 1e-6 
@@ -39,16 +39,22 @@ max_iterations = 100000
 min_convergence_iters = 99998
 
 r = 1
+optimal_wts = None  
+
 while r > 0:
     optimal_wts, history_of_cost, converged_iter = run_grad_descent(X_train_bias, y_train, initial_wts, r, tolerance, max_iterations)
     
     print(f"Learning rate {r}: Converged after {converged_iter} iterations")
-    
 
     if converged_iter < min_convergence_iters:
         print(f"Convergence achieved with learning rate {r}")
         break
     r /= 2
+
+if optimal_wts is not None:
+    print("Final Optimized Weights:", optimal_wts)
+else:
+    print("No convergence achieved.")
 
 plt.plot(history_of_cost, color='orange')  
 plt.xlabel('Iterations', fontsize=12) 
@@ -61,6 +67,5 @@ y_pred_test = np.dot(X_test_bias, optimal_wts)
 
 mse_test = (1/(2*len(y_test))) * np.sum((y_pred_test - y_test)**2)
 
-print("Final Optimized Weights:", optimal_wts)
 print("Learning Rate:", r)
 print("Test Data MSE:", mse_test)
