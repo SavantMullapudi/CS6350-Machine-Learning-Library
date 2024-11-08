36cf90d15cf0267d16ed043fbbe17836bd74341b
Savant Mullapudi <u1471518@utah.edu>
2024-11-08 13:42:51 -0700

    Create run.sh

---
+++ Perceptron/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "Perceptron Output"
+python3 perceptron_algo.py
+
+echo "Voted Perceptron Output"
+python3 voted_perceptron_algo.py
+
+echo "Average Perceptron Output"
+python3 average_perceptron_algo.py
