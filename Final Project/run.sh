66ebcc4ef31906a2f1029469b27d2d489d10c8bf
Savant Mullapudi <u1471518@utah.edu>
2024-12-13 15:57:18 -0700

    Update run.sh

--- Final Project/run.sh
+++ Final Project/run.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-echo "Income>50K probability predictions using decissionTree "
-python3 incomePredictionDecissionTree.py
+echo "Income > 50K probability predictions using decission_tree "
+python3 ml_comp_decision_tree.py
 
-echo "Income>50K probability predictions using Random Forest "
-python3 incomePredictionRandomForest.py
+echo "Income > 50K probability predictions using naive_bayes "
+python3 ml_comp_naive_bayes.py
 
-echo "Income>50K probability predictions using Adaboost "
-python3 incomePredictionAdaboost.py
+echo "Income > 50K probability predictions using adaboost "
+python3 ml_comp_adaboost.py
 
-echo "Income>50K probability predictions using Perceptron "
-python3 incomePredictionPerceptron.py
+echo "Income > 50K probability predictions using k_nearest_neighbors "
+python3 ml_comp_KNN.py
 
-echo "Income>50K probability predictions using SVM "
-python3 incomePredictionSVM.py
+echo "Income > 50K probability predictions using support_vector_machine "
+python3 ml_comp_svm.py
 
-echo "Income>50K probability predictions using Neural Network "
-python3 incomePredictionNeuralNetwork.py
+echo "Income > 50K probability predictions using logistic_regression "
+python3 ml_comp_logistic_regression.py
+
